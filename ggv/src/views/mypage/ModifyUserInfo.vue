15db02455567324085166e6539eac2504a998122
오성철 <ohbravo7@gmail.com>
2024-02-26 10:45:04 +0900

    회원수정화면 수정

--- ggv/src/views/mypage/ModifyUserInfo.vue
+++ ggv/src/views/mypage/ModifyUserInfo.vue
@@ -14,7 +14,7 @@
           <p class="conTitle">
             <span>회원정보</span>
           </p>
-          
+
           <form id="myForm" action="" method="post">
             <fieldset>
               <p>
@@ -36,10 +36,10 @@
               <p>
                 <label>휴대폰번호</label>
                 <input
-                  v-model="lgn_Id"
-                  id="EMP_ID"
+                  v-model="hp"
+                  id="hp"
                   type="text"
-                  name="lgn_Id"
+                  name="hp"
                   placeholder="010xxxxxxxx"
                   style="ime-mode: inactive"
                   @keypress="checkCode"
@@ -48,10 +48,9 @@
               <p>
                 <label>메일주소</label>
                 <input
-                  v-model="lgn_Id"
-                  id="EMP_ID"
-                  type="text"
-                  name="lgn_Id"
+                  v-model="email"
+                  id="email"
+                  name="email"
                   placeholder="xx@xx.com"
                   style="ime-mode: inactive"
                   @keypress="checkCode"
@@ -59,17 +58,24 @@
               </p>
               <p>
                 <label>비밀번호</label>
-                <a @click="modPwd()">비밀번호 변경</a>
+                <input v-model="pw" />
+                <!--                <a @click="modPwd()">비밀번호 변경</a>-->
+              </p>
+              <p>
+                <label>비밀번호확인</label>
+                <input v-model="pwConfirm" />
               </p>
               <div class="form-check" style="display: flex;">
                 <div class="radioBtn">
-                  <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" value="Y" v-model="alarmYn">
+                  <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" value="Y"
+                         v-model="alarmYn">
                   <label class="form-check-label" for="flexRadioDefault1">
                     동의
                   </label>
                 </div>
                 <div class="radioBtn" style="margin-left: 40px;">
-                  <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" value="N" v-model="alarmYn">
+                  <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" value="N"
+                         v-model="alarmYn">
                   <label class="form-check-label" for="flexRadioDefault2">
                     미동의
                   </label>
@@ -86,8 +92,8 @@
                 />
               </p>
               <!-- Login Btn -->
-              <a>수정</a>
-              <a>취소</a>
+              <button type="button" class='btn btn-primary' @click="updateUser()">수정</button>
+              <button type="button" class="btn btn-danger">취소</button>
             </fieldset>
           </form>
         </div>
@@ -98,27 +104,31 @@
 </template>
 
 <script>
-import { openModal } from "jenesius-vue-modal";
-import modifyPasswordPop from "./ModifyPasswordPop.vue";
+import { openModal } from 'jenesius-vue-modal';
+import modifyPasswordPop from './ModifyPasswordPop.vue';
 
 export default {
-  data: function () {
+  data: function() {
     return {
       items: [],
-      loginId: "",
-      userNm: "",
-      alarmYn: "N",
-      goal:"",
+      loginId: '',
+      userNm: '',
+      alarmYn: 'N',
+      goal: '',
+      email: '',
+      hp: '',
+      pw: '',
+      pwConfirm: '',
     };
   },
   methods: {
-    modPwd: async function () {
+    modPwd: async function() {
       //alert('비밀번호 변경 팝업 예정');
 
       //let action = 'I';
 
       const modal = await openModal(modifyPasswordPop, {
-        title: "비밀번호 수정",
+        title: '비밀번호 수정',
       });
 
       modal.onclose = () => {
@@ -126,10 +136,38 @@ export default {
         //return false; //Modal will not be closed
       };
     },
+
+    updateUser: async function() {
+      console.log(this.hp);
+      console.log(this.email);
+      console.log(this.pw);
+      console.log(this.pwConfirm);
+
+      if (this.pw != this.pwConfirm) {
+        alert('비밀번호와 비밀번호확인이 같지않음');
+        return;
+      }
+
+      let params = new URLSearchParams();
+      params.append('loginId', this.$store.state.loginInfo.loginId);
+      params.append('hp', this.hp);
+      params.append('email', this.email);
+      params.append('pw', this.pw);
+      params.append('alarmYn', this.alarmYn);
+
+      if (confirm('수정?')) {
+        await this.axios
+          .post('/mypage/updateUser.do', params)
+          .then(function(res) {
+            console.log(res);
+            alert('수정성공');
+          });
+      }
+    },
   },
   mounted() {
     let loginInfo = this.$store.state.loginInfo;
-    console.log("loginInfo", loginInfo)
+    console.log('loginInfo', loginInfo);
 
     this.loginId = loginInfo.loginId;
     this.userNm = loginInfo.userNm;
